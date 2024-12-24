d3c81c5ed647ad43580b689cb1419023efaa8f39
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-12-24 07:12:31 +0200

    add background-color to the last content

--- final-interview/src/pages/Content.jsx
+++ final-interview/src/pages/Content.jsx
@@ -13,23 +13,28 @@ function Content () {
      <Clients name="Running Projects" />
      </div>
   <div className="event1">
-  <div className="event2">
-    <div className="up-coming">
-     <Events  name="Upcoming Events"/>
-     </div>
-     <Events  name="Marketing policy "/>
+    <div className="event2">
+      <div className="up-coming">
+       <Events  name="Upcoming Events"/>
+        </div>
+
+        <div className="last-content">
+         <Events  name="Marketing policy "/>
+       </div>
     
      </div>
-     <div  className="event3">
-     <div className="up-coming">
-     <Events  name="Running Projects"/>
+       <div  className="event3">
+        <div className="up-coming">
+         <Events  name="Running Projects"/>
      </div>
-     <div>
-     <Events  name="Database configuration"/>
-     </div>
-     </div>
-     </div>
-   </div>
+       <div>
+        <div className="last-content">
+         <Events  name="Database configuration"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
   
     )
   }
