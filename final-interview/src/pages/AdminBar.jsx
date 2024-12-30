7337852f5c9cdefc258f1cc5cc3f98fe54cef598
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-12-30 08:05:53 +0200

    Add style to admin bar

--- final-interview/src/pages/AdminBar.jsx
+++ final-interview/src/pages/AdminBar.jsx
@@ -3,11 +3,16 @@ import { BsStopwatch } from "react-icons/bs";
 function AdminBar () {
 
     return (
+      <>
    <div className="admin-bar">
             <div className="span5"><BsStopwatch /></div>
+            <div>
     <h1>CRM Admin Dashboard</h1>
     <p>very detailed & featured admin</p>
+    </div>
    </div>
+
+</>
 
     )
   }
